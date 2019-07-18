9d738dc33eb6a2e7ad7c89d614b0f90952b5b02d
emanuel <lelouch1114@gmail.com>
2019-07-17 21:34:11 -0500

    Added the entire project to he repository
    Emanuel Restrepo

---
+++ Assets/Scripts/WaypointMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointMovement : MonoBehaviour {
+	
+	public GameObject player;
+    public GameObject Waypoint;
+    public GameObject TemporalWaypoint;
+
+    public float fireRate;
+    private float nextFire;
+
+
+    public float height = 2;
+	public bool teleport = true;
+
+	public float maxMoveDistance = 10;
+	private bool moving = false;
+
+    public void CallTeleport(GameObject temporalwaypoint)
+    {
+        TemporalWaypoint = temporalwaypoint;
+        StartCoroutine(Teleporting());
+    }
+
+    public void CancelTeleport()
+    {
+        TemporalWaypoint = null;
+        StopAllCoroutines();
+    }
+
+    public IEnumerator Teleporting()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        if (Waypoint != null) Waypoint.SetActive(true);
+
+        player.transform.position = new Vector3(TemporalWaypoint.transform.position.x,
+            TemporalWaypoint.transform.position.y + height / 2,
+            TemporalWaypoint.transform.position.z);
+        Waypoint = TemporalWaypoint;
+
+        if (Waypoint != null) Waypoint.SetActive(false);
+
+    }
+
+}
+
